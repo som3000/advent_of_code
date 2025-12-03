9972ab91729d05a5e22ddb87349d0a93dd8ce40c
Som Nath Gupta <somnathgupta@Soms-MacBook-Pro.local>
2025-12-03 11:54:04 +0530

    Day 2 part 1 done

--- day2/p1.js
+++ day2/p1.js
@@ -1,3 +1,38 @@
 const data = Deno.readTextFileSync("./day2Data.txt");
-const dataArray = data.split(",").map(x => parseInt(x))
-console.log(dataArray);
+const dataArray = data.split(",").map((x) => parseInt(x));
+dataArray[1]=12;
+dataArray[2]=2;
+
+const add = (array, i) => {
+  array[array[i + 3]] = array[array[i + 1]] + array[array[i + 2]];
+};
+
+const multiply = (array, i) => {
+  array[array[i + 3]] = array[array[i + 1]] * array[array[i + 2]];
+};
+
+const redirect = (array, i) => {
+  switch (array[i]) {
+    case 1:
+      add(array, i);
+      break;
+    case 2:
+      multiply(array, i);
+      break;
+
+    default:
+      break;
+  }
+};
+
+const intComputer = (dataArray) => {
+  const iterationArray = dataArray.slice();
+
+  for (let i = 0; iterationArray[i] !== 99; i = i + 4) {
+    redirect(iterationArray, i);
+  }
+
+  return iterationArray.join(",");
+};
+
+console.log(intComputer(dataArray));
