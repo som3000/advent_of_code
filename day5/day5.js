596ca4e2c069805118ea0e354759eb57225dd7ef
Som Nath Gupta <somnathgupta@Soms-MacBook-Pro.local>
2025-12-04 13:02:05 +0530

    Day 5 part 1 done and made it cleaner.

--- day5/day5.js
+++ day5/day5.js
@@ -1,51 +1,62 @@
-// const bigdata = Deno.readTextFileSync("./day5.txt");
-// const bigdataArray = bigdata.split(",");
+const bigdata = Deno.readTextFileSync("./day5.txt");
+const bigdataArray = bigdata.split(",").map((x) => parseInt(x));
 
-const array = [3,2,10,2,3,3,4,2,99];
+// const smallData = "1,2,3,4,5";
+// const smallDataArray = smallData.split(",").map((x) => parseInt(x));
 
-const add = (array, i) => {
-  array[array[i + 3]] = array[array[i + 1]] + array[array[i + 2]];
+const formatExecutionCode = (code) => {
+  const codeStr = code += "";
+  const paddedStr = codeStr.padStart(5, "0");
+  return ({
+    opCode: parseInt(paddedStr.slice(-2)),
+    para1Mode: parseInt(paddedStr[2]),
+    para2Mode: parseInt(paddedStr[1]),
+    para3Mode: parseInt(paddedStr[0]),
+  });
 };
 
-const multiply = (array, i) => {
-  array[array[i + 3]] = array[array[i + 1]] * array[array[i + 2]];
+const inputAccToMode = (array, i, mode) => {
+  if (mode === 0) return array[i];
+  return i;
 };
+
+const add = (array, i, { para1Mode, para2Mode, para3Mode }) => {
+  array[inputAccToMode(array, i + 3, para3Mode)] =
+    array[inputAccToMode(array, i + 1, para1Mode)] +
+    array[inputAccToMode(array, i + 2, para2Mode)];
+};
+
+const multiply = (array, i, { para1Mode, para2Mode, para3Mode }) => {
+  array[inputAccToMode(array, i + 3, para3Mode)] =
+    array[inputAccToMode(array, i + 1, para1Mode)] *
+    array[inputAccToMode(array, i + 2, para2Mode)];
+};
+
+const input = (array, i, { para1Mode }) => {
+  array[inputAccToMode(array, i + 1, para1Mode)] = 1;
+};
+
+const output = (array, i, { para1Mode }) =>
+  console.log(array[inputAccToMode(array, i + 1, para1Mode)]);
 
 const redirect = (array, i) => {
-  switch (array[i]) {
-    case 1:
-      add(array, i);
-      break;
-    case 2:
-      multiply(array, i);
-      break;
-    case 3:
-      array[array[i+1]] = 1;
-      break;
+  const formattedEx = formatExecutionCode(array[i]);
+  const operations = { 1: add, 2: multiply, 3: input, 4: output };
 
-    case 4:
-      console.log(array[i+1]);
-      break;
-
-    default:
-      break;
-  }
+  operations[formattedEx.opCode](array, i, formattedEx);
 };
 
-const indexInc = (number, i) => {
-  if (number >2) return i +2;
-  return i + 4; 
-}
+const indexInc = (exCode, i) => {
+  if ("34".includes(exCode + "")) return i + 2;
+  return i + 4;
+};
 
 const intComputer = (dataArray) => {
-  let i = 0
+  let i = 0;
   while (dataArray[i] !== 99) {
     redirect(dataArray, i);
-    i = indexInc(dataArray[i], i);
+    i = indexInc(dataArray[i] % 10, i);
   }
-
-  return dataArray;
 };
 
-intComputer(array);
-
+intComputer(bigdataArray);
