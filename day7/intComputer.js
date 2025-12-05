4daee05587235fca261da052aea61b05fd32b3f7
Som Nath Gupta <somnathgupta@Soms-MacBook-Pro.local>
2025-12-05 15:42:13 +0530

    Comented data not in use to remove lint

--- day7/intComputer.js
+++ day7/intComputer.js
@@ -1,6 +1,6 @@
-const smallData = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
+// const smallData = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
 
-const smallDataArray = smallData.split(",").map((x) => parseInt(x));
+// const smallDataArray = smallData.split(",").map((x) => parseInt(x));
 
 const inputAccToMode = (array, i, mode) => {
   if (mode === 1) {
