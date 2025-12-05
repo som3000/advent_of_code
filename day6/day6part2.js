a575b7e84e36dce739cd046e7fe870bc56174a98
Som Nath Gupta <somnathgupta@Soms-MacBook-Pro.local>
2025-12-05 11:18:38 +0530

    DAy 6 part 2 Done.

--- day6/day6part2.js
+++ day6/day6part2.js
@@ -13,23 +13,47 @@ K)YOU
 I)SAN`;
 const orbitalData= Deno.readTextFileSync("./day6data.txt")
 
-const breakOrbitToArray = str => {
-  return str.split("\n").map(x => x.split(")"))
-}
+const breakOrbitToArray = (str) => {
+  return str.split("\n").map((x) => x.split(")"));
+};
 
-const breakOrbitsIntoObject = str => {
+const breakOrbitsIntoObject = (str) => {
   const array = breakOrbitToArray(str);
-  const orbits ={};
-  for(let i = 0; i< array.length; i++) {
-    orbits[array[i][1]] =  array[i][0]
+  const orbits = {};
+  for (let i = 0; i < array.length; i++) {
+    orbits[array[i][1]] = array[i][0];
   }
   return orbits;
-}
+};
 
+const pathToCom = (orbitsObject, startPoint) => {
+  let currentPlanet = orbitsObject[startPoint];
+  const path = [];
+  while (currentPlanet !== "COM") {
+    path.push(currentPlanet);
+    currentPlanet = orbitsObject[currentPlanet];
+  }
+  return path;
+};
 
+const commonPoint = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i])) return array1[i];
+  }
+};
 
-console.log(orbitsTransfer(breakOrbitsIntoObject(planetsOrbit)));
+const orbitsTransfer = (orbitsObject) => {
+  const youPathToCom = pathToCom(orbitsObject, "YOU");
+  const sanPathToCom = pathToCom(orbitsObject, "SAN");
+  const commonPlanet = commonPoint(youPathToCom, sanPathToCom);
+  const youDistToCommonPoint =
+    youPathToCom.slice(0, youPathToCom.indexOf(commonPlanet) + 1).length;
+  const sanDistToCommonPoint =
+    sanPathToCom.slice(0, sanPathToCom.indexOf(commonPlanet) + 1).length;
+  return youDistToCommonPoint + sanDistToCommonPoint - 2;
+};
 
+console.log(orbitsTransfer(breakOrbitsIntoObject(orbitalData)));
 
 // const planetTotalOrbit = (planet,orbits,counter) => {
 //   if(orbits[planet] === undefined) return counter;
